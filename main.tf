108bf9723a4fba773bfeb2e2902ee4edb839a5b8
bandu0307 <gautamharshb@gmail.com>
2026-02-13 09:44:56 +0530

    Initialize Terraform with Azure provider and resource group
    Add Terraform configuration for Azure provider and resource group.

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
 
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+feature {
+
+}
+subscription_id = "dd97ffc7-2727-4b3a-b9c9-737999390c35"
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg57" {
+  name     = "rg-57"
+  location = "West Europe"
+}
